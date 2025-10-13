ed8b85da5ae7eb4a8db620b49833755cff5193c0
Jayant Kashyap <114590622+Jayant0403@users.noreply.github.com>
2025-10-13 17:26:44 +0530

    Update SpringbootBackendApplicationTests.java

--- springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
+++ springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@ActiveProfiles("test")
+public class SpringbootBackendApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 }
