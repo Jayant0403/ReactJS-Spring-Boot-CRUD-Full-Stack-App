6b6373280d3f466129d554582f6aea8d29100eb5
Jayant Kashyap <114590622+Jayant0403@users.noreply.github.com>
2025-10-14 10:59:21 +0530

    Update SpringbootBackendApplicationTests.java

--- springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
+++ springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -1,7 +1,7 @@
 package net.javaguides.springboot;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.ActiveProiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
